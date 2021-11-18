14cad72f0520db2cef227638b0853381351883bc
CodeClan Student <7378035+NodeToNowhere@users.noreply.github.com>
2021-11-18 11:35:21 +0000

    tdd development

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,11 @@
-def add_one(num):#not inside class don't need self
+def add_one(num):  # not inside class don't need self
     return num + 1
-    
+
+
+def compare(num1, num2):
+    if num1 > num2:
+        return "first number greater than"
+    elif num1 < num2:
+        return "first number less than"
+    elif num1 == num2:
+        return "first number equal to"
--- tests/function_tests.py
+++ tests/function_tests.py
@@ -1,10 +1,26 @@
 import unittest
 from src.functions import *
 
+
 class TestFunctions(unittest.TestCase):
-    
-    def test_add_one__5_returns_6(self):#super clear defined test names
-        #3 A's Arrage, Act, Assert
+    def test_add_one__5_returns_6(self):  # super clear defined test names
+        # 3 A's Arrage, Act, Assert
         expected = 6
-        actual = add_one(5) #the number I'll get back with the arg used
-        self.assertEqual(expected,actual)
+        actual = add_one(5)  # the number I'll get back with the arg used
+        self.assertEqual(expected, actual)
+
+    def test_compare__3_is_greater_than_one(self):
+        expected = "first number greater than"
+        actual = compare(3, 1)
+        self.assertEqual(expected, actual)
+
+    def test_compare__3_is_less_than_5(self):
+        expected = "first number less than"
+        actual = compare(3, 5)
+        self.assertEqual(expected, actual)
+        
+    def test_compare__3_is_equal_to(self):
+        expected = "first number equal to"
+        actual = compare(3, 3)
+        self.assertEqual(expected, actual)
+
